b1fff41c2617c9490ff6078301bd7b5b34924196
henrique <ehenriique07@gmail.com>
2022-07-12 15:00:56 -0300

    Reenvio de Atividade

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 var dataEvento = "";
 var idadeDoParticipante= 0;
-var quantidadePessoas=0;
-var listaDeConvidados="";
-var listaDePalestrante ="";
+var listaParticipantes= ["Convidado 1", "Convidado 2 ", "Convidado 3", "Convidado 4"]
+var palestrante= "Henrique";
 
-dataEvento = prompt ("Informe dia do Evento")
-if (dataEvento != "Hoje"){
- console.log ("Evento Permitido")
+var hoje = new Date();
+var diaEvento= prompt ("Qual é o Dia para realizar o Evento ? (dd)");
+var mesEvento= prompt ("Qual o mês do Evento ? (mm)");
+var anoEvento= prompt ("Qual é o ano do Evento ?(aaaa)");
+var dataEvento= new Date (anoEvento, mesEvento -1 , diaEvento)
+
+if (dataEvento <=hoje){
+   console.log ("Data do Evento Invalida")
 } else {
- console.log("Dia Invalido")
+   console.log ("Evento Permitido")
 }
 
-idadeDoParticipante = prompt ("Qual sua idade ?")
+idadeDoParticipante = prompt ("Qual a idade do Participante ?")
 if (idadeDoParticipante >= 18){
  console.log ("Permitido Cadastro")
-} else{
- console.log ("Idade não permitida")
-}
-
-quantidadePessoas = prompt ("Quantidade de Pessoas?")
-if (quantidadePessoas <= 100){
- console.log ("Permitido")
-} else {
-   console.log ("Quantidade Maxima de Pessoas Atingido.")
-}
-
-
-listaDeConvidados = prompt ("Você é Convidado ou Palestrante?")
-if (listaDeConvidados == "Convidado"){
- console.log ("Bem-vindo Convidado")
-} else {
- console.log ("Bem-vindo Palestrante")
-}
-
-
+}  else{
+   if (listaParticipantes < 100){
+      console.log ("Lista de Participantes ")
+   } else {
+      console.log ("Lista Cheia")
+   }
    
+      console.log ("Idade não permitida")
+}
+
+
+console.log ("O palestrante do Evento é " + palestrante );
+console.log ("Quantidade de Participantes Cadastrados é ", listaParticipantes.length,)
+   for ( let i = 0; i<listaParticipantes; i++){
+      console.log (listaParticipantes[i]);
+   }
